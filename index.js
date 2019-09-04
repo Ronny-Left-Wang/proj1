754597a0c167edcb9c54587d322e26de49fc8ba7
Ronnapob Ritprasert <Ronnyrit@gmail.com>
2019-09-03 18:28:03 -0700

    Made header and footer look better, website now has a jumbotron title

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const app = require('express')();
 const path = require('path');
+const hbs = require('hbs');
 
 const User = require('./models/User');
 const Post = require('./models/Post');
 
-const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','hbs');
-
-hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res) => {
     res.render('index', { users, posts, layout: 'layouts/default' });
